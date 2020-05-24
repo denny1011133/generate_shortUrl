0138811fbf9906d9838d7f610aa5176adceb7051
Denny <denny1011133@gmail.com>
2020-05-24 13:30:33 +0800

    add mongoose

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-// 載入 express 並建構應用程式伺服器
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/url', { useNewUrlParser: true, useUnifiedTopology: true })
 
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 // 設定首頁路由
 app.get('/', (req, res) => {
   res.send('hello world')
